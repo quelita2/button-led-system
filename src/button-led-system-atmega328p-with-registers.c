72854158eb23e45fcdd7e5e17ca74b8de98f3314
Thiago Lopes <96271397+thiagonasmto@users.noreply.github.com>
2023-09-13 21:56:39 -0300

    Update button-led-system-atmega328p-with-registers.c

--- src/button-led-system-atmega328p-with-registers.c
+++ src/button-led-system-atmega328p-with-registers.c
@@ -1,8 +1,3 @@
-#include <util/delay.h>
-
-int buttonPin = 3;      // Define buttonPin no pino digital 3
-int ledPin = 4;         // Define ledPin no pino digital 4
-
 int main(void) {
     DDRD |= (1 << DDD4);     // Configura ledPin (pino 4) como saída
     PORTD &= ~(1 << PORTD4); // Desliga o LED inicialmente
